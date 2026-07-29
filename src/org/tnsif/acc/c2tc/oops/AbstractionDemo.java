2ec1668096fd4cd89ffb120725ea6a72b792808a
Mythri V H <mythrivhuded@gmail.com>
2026-07-29 12:38:31 +0530

    first commit

---
+++ src/org/tnsif/acc/c2tc/oops/AbstractionDemo.java
@@ -0,0 +1,19 @@
+package org.tnsif.acc.c2tc.oops;
+
+class MaheshDem{
+	void calling() {
+		
+	}
+}
+
+
+
+
+public class AbstractionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/tnsif/acc/c2tc/oops/HirearichalInheritance.java
@@ -0,0 +1,10 @@
+package org.tnsif.acc.c2tc.oops;
+
+public class HirearichalInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
