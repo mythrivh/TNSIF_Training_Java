72f1f323e155bd9a0267f6164d07132966d7c1ca
Mythri V H <mythrivhuded@gmail.com>
2026-08-11 12:39:37 +0530

    first commit

--- src/org/tnsif/acc/c2tc/markerinterface/Student.java
+++ src/org/tnsif/acc/c2tc/markerinterface/Student.java
@@ -1,5 +1,34 @@
 package org.tnsif.acc.c2tc.markerinterface;
 
 public class Student {
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getFees() {
+		return fees;
+	}
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	private String name;
+	private double fees;
+	private String course;
 
 }
